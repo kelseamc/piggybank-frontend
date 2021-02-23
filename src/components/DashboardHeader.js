d678f2f1c58be53aa365db6d395d226724b2c574
kelseamc <kelsea.mcall@gmail.com>
2021-02-23 13:26:50 -0500

    madjusted CSS

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -2,24 +2,17 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import DashChart from './DashChart'
 
-
-
-
-
 function DashboardHeader({setFilter, filter}) {
     const userName = useSelector((state) => state.user.name)
     const toBe = useSelector((state) => state.balance.assign)
    
-
-
-
     return (
         <div className="dash-top">
            
-            <div id="chart">
+            <div className="header-tile" id="chart">
                 <DashChart setFilter={setFilter} filter={filter}/>
             </div>
-            <div className="head-text">
+            <div className="header-tile" id="head-text">
                 <h1>Welcome {userName}!</h1>
                 <br />
                 <h3>Left to be Assigned: ${toBe.toFixed(2)}</h3>
